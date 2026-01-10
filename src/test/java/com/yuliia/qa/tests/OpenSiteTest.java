7f87f9fcc3c4009d3fb3f80f7d829d4e31ea1d05
Yuliia <julia.koshovets@gmail.com>
2026-01-09 19:43:29 -0800

    Introduced BaseTest class and removed duplicated WebDriver code from tests

--- src/test/java/com/yuliia/qa/tests/OpenSiteTest.java
+++ src/test/java/com/yuliia/qa/tests/OpenSiteTest.java
@@ -6,17 +6,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import com.yuliia.qa.core.BaseTest;
 
-public class OpenSiteTest {
+public class OpenSiteTest extends BaseTest{
     @Test
     public void openSauceDemoHomePage(){
-        WebDriver driver = new ChromeDriver();
-        try {
             driver.get("https://www.saucedemo.com/"); // Open page
             WebElement loginButton = driver.findElement(By.id("login-button"));
             Assert.assertTrue(loginButton.isDisplayed(), "Login button should be visible on the login page");
-        } finally {
-            driver.quit();
-        }
     }
 }
